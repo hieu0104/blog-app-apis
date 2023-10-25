b2ee13802fe52debb0aaa70eea15eeb5d2a3f060
hieu0104 <lechihieubl2020@gmail.com>
2023-10-25 23:57:20 +0700

    change config swagger

--- src/main/java/com/hieu/blog/config/SpringDocConfig.java
+++ src/main/java/com/hieu/blog/config/SpringDocConfig.java
@@ -15,77 +15,30 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.security.core.context.SecurityContext;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+
 @Configuration
-
-@OpenAPIDefinition(
-        info = @io.swagger.v3.oas.annotations.info.Info(
-
-                contact = @Contact(
-
-                        name = "HieuLe",
-
-                        email = "lechihieu.at.@gamail.com"
-
-
-                ),
-
-                description = " Blog app apis ",
-
-                title = "  Blog app apis backend "
-
-
-        )
-      /*  ,
-        servers = {
-        @Server(url = "", description = "Default Server URL")
-
-}
- */
-
-
-
-
-
-
-)
-
 public class SpringDocConfig {
-   /* @Bean
-    public OpenAPI apiInfo() {
-        return new OpenAPI().info(new Info().title("Blog app apis").version("1.0.0"));
-    }*/
-
-   /* @Bean
-    public GroupedOpenApi httpApi() {
-        return GroupedOpenApi.builder()
-                .group("http")
-                .pathsToMatch("/**")
-                .build();
-    }*/
-
-
-    private SecurityScheme createAPIKeyScheme() {
-        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
-                .bearerFormat("JWT")
-                .scheme("bearer");
-    }
-
     @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI().addSecurityItem(new SecurityRequirement().
-                        addList("Bearer Authentication"))
-                .components(new Components().addSecuritySchemes
-                        ("Bearer Authentication", createAPIKeyScheme()))
-                .info(new Info().title("My REST API")
-                        .description("Some custom description of API.")
-                        .version("1.0").contact(new io.swagger.v3.oas.models.info.Contact().name("Sallo Szrajbman")
-                                .email("www.baeldung.com").url("salloszraj@gmail.com"))
-                        .license(new License().name("License of API")
-                                .url("API license URL")));
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Your API Title")
+                        .version("Your API version")
+                        .description("Your API description"))
+                .components(new Components()
+                        .addSecuritySchemes("bearer-jwt", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearer-jwt", new ArrayList<String>(Arrays.asList("read", "write"))));
+
+
     }
+
 
 }
